90a2fd9e6df208284b2920d78a25b454a775dbc2
MauroJavier0904 <mauroflores0904@gmail.com>
2025-09-08 08:04:21 -0700

    Wipe repository contents

--- Documents/gameplannr/supabase/migrations/20250907191000_add_additional_sport_column.sql
+++
@@ -1,2 +0,0 @@
--- Add additional_sport column to profiles table
-ALTER TABLE profiles ADD COLUMN additional_sport TEXT;
--- Documents/gameplannr/supabase/migrations/20250907192000_allow_null_age.sql
+++
@@ -1,2 +0,0 @@
--- Allow NULL values for age column in profiles table
-ALTER TABLE profiles ALTER COLUMN age DROP NOT NULL;
